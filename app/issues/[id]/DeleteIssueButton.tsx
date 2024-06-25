21b60f2643321698152e2d716f7934e147809fe2
Ehsan Ahmadvand <Ehsan110313@gmail.com>
2024-06-25 16:52:03 +0430

    Show a confirmation dialog box

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant='soft' color="gray">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
+
+export default DeleteIssueButton;
