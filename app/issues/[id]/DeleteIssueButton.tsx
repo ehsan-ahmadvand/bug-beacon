88a9270197a0c19134098de3c8a6ad49270c83f0
Ehsan Ahmadvand <Ehsan110313@gmail.com>
2024-06-29 01:57:37 +0430

    Add a delete button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,29 +1,9 @@
-'use client';
+import { Button } from '@radix-ui/themes'
 
-import { AlertDialog, Button, Flex } from '@radix-ui/themes';
-
-const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+const DeleteIssueButton = ({ issueId }: { issueId: number}) => {
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action cannot be undone.
-        </AlertDialog.Description>
-        <Flex mt="4" gap="3">
-          <AlertDialog.Cancel>
-            <Button variant='soft' color="gray">Cancel</Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color="red">Delete Issue</Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
-  );
-};
+    <Button color="red">Delete Issue</Button>
+  )
+}
 
-export default DeleteIssueButton;
+export default DeleteIssueButton
