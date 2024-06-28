5c63bcfc51989303ed3ebf2e6aa2cf83854e9aa5
Ehsan Ahmadvand <Ehsan110313@gmail.com>
2024-06-29 04:01:28 +0430

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-       return [
-          {
-             source: '/:path*',
-             headers: [
-                { key: 'referrer-policy', value: 'no-referrer'}
-             ]
-          }
-       ]
-    }
- }
- 
- module.exports = nextConfig
+   async headers() {
+      return [
+         {
+            source: '/:path*',
+            headers: [
+               { key: 'referrer-policy', value: 'no-referrer'}
+            ]
+         }
+      ]
+   }
+};
+
+export default nextConfig;
