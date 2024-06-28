d9060e6722c4dab42a2794f586ecbf3c40c0f6e1
Ehsan Ahmadvand <Ehsan110313@gmail.com>
2024-06-29 01:47:49 +0430

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,7 +36,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data);
+      if (issue) await axios.patch('/api/issues/' + issue.id, data);
+      else await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
       setSubmitting(false);
@@ -52,7 +53,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         </Callout.Root>
       )}
       <form className="space-y-3" onSubmit={onSubmit}>
-        <TextField.Root defaultValue={issue?.title}
+        <TextField.Root  defaultValue={issue?.title}
             placeholder="Title"
             {...register('title')}>
         </TextField.Root>
@@ -67,7 +68,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
