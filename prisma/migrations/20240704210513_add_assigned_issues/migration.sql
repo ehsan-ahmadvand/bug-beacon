c14f9b14ea3278e39a8d3a24b22baee8220636d6
Ehsan Ahmadvand <Ehsan110313@gmail.com>
2024-07-05 01:35:45 +0430

    Add assigned issues to prisma schema

---
+++ prisma/migrations/20240704210513_add_assigned_issues/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `issue` ADD COLUMN `assignedToUserId` VARCHAR(255) NULL;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedToUserId_fkey` FOREIGN KEY (`assignedToUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
